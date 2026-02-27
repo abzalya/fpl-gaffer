536f496d632cdd7a7ed38bda28273bb2e1f7d1ba
Abzal Amirbay <aamirbay@me.com>
2026-02-27 00:29:36 +0000

    dbt error: ambiguous opta_code Fixed.

--- 02-dbt/macros/fixture_horizon.sql
+++ 02-dbt/macros/fixture_horizon.sql
@@ -3,11 +3,11 @@
     {{ fixture_alias }}.difficulty fixture_difficulty_h{{ horizon }},
     coalesce(
         {{ fixture_alias }}.is_home,
-        lead(was_home, {{ horizon }}) over (partition by opta_code, season_id order by gameweek_id)
+        lead(pgb.was_home, {{ horizon }}) over (partition by pgb.opta_code, pgb.season_id order by pgb.gameweek_id)
     ) fixture_is_home_h{{ horizon }},
     coalesce(
         case when {{ fixture_alias }}.is_home then {{ fixture_alias }}.team_a else {{ fixture_alias }}.team_h end,
-        lead(opponent_team_id, {{ horizon }}) over (partition by opta_code, season_id order by gameweek_id)
+        lead(pgb.opponent_team_id, {{ horizon }}) over (partition by pgb.opta_code, pgb.season_id order by pgb.gameweek_id)
     ) opponent_team_id_h{{ horizon }},
-    lead(opponent_strength, {{ horizon }}) over (partition by opta_code, season_id order by gameweek_id) opponent_strength_h{{ horizon }}
+    lead(pgb.opponent_strength, {{ horizon }}) over (partition by pgb.opta_code, pgb.season_id order by pgb.gameweek_id) opponent_strength_h{{ horizon }}
 {% endmacro %}
